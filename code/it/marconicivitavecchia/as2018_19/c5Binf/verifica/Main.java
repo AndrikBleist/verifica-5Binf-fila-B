2389daddbf03e893090e8f6e37a13c60f264e425
AndrikBleist <erikandronico@gmail.com>
2018-11-20 11:25:02 +0100

    Consegna Compito

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package Compito;
 
 import javax.swing.*;
 
@@ -12,16 +12,35 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(640,480);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		StringBuilder sb= new StringBuilder();
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<html>");
+		sb.append("<head><h1><b>Live the Company</h1></head>");
+		sb.append("<body>");
+		sb.append("<table>");
+		sb.append("<tr><th><h2><b>Evento</b></h2></th>");
+		sb.append("<th><h2><b>Data</b></h2></th>");
+		sb.append("<th><h2><b>Vado?</b></h2></th></tr>");
+		sb.append("<tr><th>Cocktail party</th>");
+		sb.append("<th>7 dic</th>");
+		sb.append("<th>Si</th></tr>");
+		sb.append("<tr><th>Colazione sociale</th>");
+		sb.append("<th>12 dic</th>");
+		sb.append("<th>Si</th></tr>");
+		sb.append("<tr><th>Saluti di Natale</th>");
+		sb.append("<th>12 dic</th>");
+		sb.append("<th>No</th>");
+		sb.append("</table>");
+		sb.append("</body>");
+		sb.append("</html>");
 		
 		// TODO Convertire lo StringBuilder in String
-		
+		String html= sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(html));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
